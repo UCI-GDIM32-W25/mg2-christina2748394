a80e485c69ecb0e156c861fd5ee24368596c7747
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-01-13 18:37:52 -0800

    add ice a penguin jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float jumpSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, jumpSpeed);
+        }
+    }
+}
